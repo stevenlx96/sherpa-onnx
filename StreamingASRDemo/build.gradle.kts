cf1fbbdba6d17cb4d4dfa3c94ca0544e0cbcd2d1
stevenlx96 <stevenlx96@gmail.com>
2025-12-17 17:01:52 +0800

    removed unused demo

--- StreamingASRDemo/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
